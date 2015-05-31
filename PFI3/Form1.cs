d677440f687fcf4f4a78beba8b603b98d4017212
davidrodier <david_rodier@hotmail.com>
2015-05-30 20:43:45 -0400

    Interface

--- PFI3/Form1.cs
+++ PFI3/Form1.cs
@@ -16,5 +16,83 @@ namespace PFI3
         {
             InitializeComponent();
         }
+
+        private void RDB_F1_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Enabled = 
+            label2.Enabled = 
+            TXB_A.Enabled = 
+            TXB_B.Enabled = 
+            BTN_Calculer.Enabled = 
+            (RDB_F1.Checked ||
+             RDB_F2.Checked ||
+             RDB_F3.Checked ||
+             RDB_F4.Checked ||
+             RDB_F5.Checked);
+        }
+
+        private void RDB_F2_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Enabled =
+            label2.Enabled =
+            TXB_A.Enabled =
+            TXB_B.Enabled =
+            BTN_Calculer.Enabled =
+            (RDB_F1.Checked ||
+             RDB_F2.Checked ||
+             RDB_F3.Checked ||
+             RDB_F4.Checked ||
+             RDB_F5.Checked);
+        }
+
+        private void RDB_F3_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Enabled =
+            label2.Enabled =
+            TXB_A.Enabled =
+            TXB_B.Enabled =
+            BTN_Calculer.Enabled =
+            (RDB_F1.Checked ||
+             RDB_F2.Checked ||
+             RDB_F3.Checked ||
+             RDB_F4.Checked ||
+             RDB_F5.Checked);
+        }
+
+        private void RDB_F4_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Enabled =
+            label2.Enabled =
+            TXB_A.Enabled =
+            TXB_B.Enabled =
+            BTN_Calculer.Enabled =
+            (RDB_F1.Checked ||
+             RDB_F2.Checked ||
+             RDB_F3.Checked ||
+             RDB_F4.Checked ||
+             RDB_F5.Checked);
+        }
+
+        private void RDB_F5_CheckedChanged(object sender, EventArgs e)
+        {
+            label1.Enabled =
+            label2.Enabled =
+            TXB_A.Enabled =
+            TXB_B.Enabled =
+            BTN_Calculer.Enabled =
+            (RDB_F1.Checked ||
+             RDB_F2.Checked ||
+             RDB_F3.Checked ||
+             RDB_F4.Checked ||
+             RDB_F5.Checked);
+        }
+
+        private void BTN_Clear_Click(object sender, EventArgs e)
+        {
+            TXB_A.Clear();
+            TXB_B.Clear();
+
+            RDB_F1.Checked = RDB_F2.Checked = RDB_F3.Checked = RDB_F4.Checked = RDB_F5.Checked = false;
+        }
     }
 }
